48403c41910904868614369276c9cfa390ba8369
rayuthuva05 <139629503+rayuthuva05@users.noreply.github.com>
2024-12-01 09:46:01 +0000

    tables modified

--- Practicals/Prac22_11/getTable_fields.php
+++ Practicals/Prac22_11/getTable_fields.php
@@ -4,40 +4,34 @@ function PrintTableCols($tableName,$connect,$colnames)
 {
 	try {
 
-		//Query
 		$sql = "SELECT ";
 		for ($i=0; $i < sizeof($colnames)-1; $i++) { 
 			$sql .=$colnames[$i].",";
-			//echo $sql;
 		}
 		$sql .=$colnames[sizeof($colnames)-1]." from $tableName";
-		//echo $sql;
-		//excute the quey
-
+		
 		$result = mysqli_query($connect,$sql);
-		//check if data exists in the table
 		if (mysqli_num_rows($result)>0) {
-			// fetch the data from rows
 			echo "<table border='1'>";
 			$col = mysqli_fetch_fields($result);
-			//print the colums
+            $cols=sizeof($colnames);
+            echo "<tr>";
+            echo "<th colspan=$cols>$tableName</th>";
+            echo "</tr>";
 			echo "<tr>";
 			foreach ($col as $value) {
-				//return an object
-				//print_r($value);
 				echo "<td>$value->name</td>";
 			}
 			echo "</tr>";
 			
 			while ($row = mysqli_fetch_assoc($result)) {
-				//print the data in a table format
 				echo "<tr>";
 				foreach ($row as $key => $value) {
 					echo "<td>$value</td>";
 				}
 				echo "</tr>";
 			}
-			echo "</table>";
+			echo "</table>"."<br>";
 		} else {
 			echo "No results";
 		}
