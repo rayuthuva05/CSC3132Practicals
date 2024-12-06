170def734193be9a5c0de600a3fb703d0b3a7e1c
rayuthuva05 <139629503+rayuthuva05@users.noreply.github.com>
2024-12-06 08:35:32 +0000

    some files are created

---
+++ prac06_12/dbconf.php
@@ -0,0 +1,19 @@
+<?php
+define('SERVERNAME', '127.0.0.1');
+define('USERNAME', 'root');
+define('PASSWORD', 'mariadb');
+define('DBNAME', 'book');
+try {
+	$connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
+	if (!$connect) {
+		die("connection failed".mysqli_connect_error());
+	} 
+	else {
+		echo "Connected successfully<br>";
+	}
+} 
+catch (Exception $e) {
+	die($e->getMessage());
+}
+
+?>
